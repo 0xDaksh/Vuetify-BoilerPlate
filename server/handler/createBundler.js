4ae5a604d1642fec4d680a611aa3bce126fe8146
Daksh Miglani <hello@dak.sh>
2017-08-14 20:07:45 +0530

    fixes

--- server/handler/createBundler.js
+++ server/handler/createBundler.js
@@ -1,4 +1,10 @@
 import { createBundleRenderer } from 'vue-server-renderer'
+import fs from 'fs'
+import path from 'path'
+import LRU from 'lru-cache'
+const resolve = file => path.resolve(__dirname, file)
+// IMPORT HTML TEMPLATE
+const template = fs.readFileSync(resolve('../../assets/index.template.html'), 'utf-8')
 export default function createRenderer (bundle, options) {
   // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
   return createBundleRenderer(bundle, Object.assign(options, {
