2771912f56f93181a3bcd4c059ccbcc485416ee7
Daksh <hello@dak.sh>
2017-08-16 19:30:09 +0530

    Update setup.js

--- server/handler/setup.js
+++ server/handler/setup.js
@@ -4,8 +4,8 @@ import favicon from 'serve-favicon'
 export default (app, serve) => {
   app.use(compression({ threshold: 0 }))
   app.use(favicon(__dirname + '/../../static/favicon.ico'))
-  app.use('/static', serve('../../static', true))
-  app.use('/public', serve('../../public', true))
+  app.use('/static', serve('../static', true))
+  app.use('/public', serve('../public', true))
   app.use('/static/robots.txt', serve('../../robots.txt'))
   app.get('/sitemap.xml', (req, res) => {
     res.setHeader("Content-Type", "text/xml")
